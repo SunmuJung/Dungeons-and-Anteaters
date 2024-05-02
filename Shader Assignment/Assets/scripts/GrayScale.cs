e134ec79d96fe807965901291dc9dcef34b1ef36
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2024-05-01 21:19:36 -0700

    Revert "HealthBar"
    This reverts commit 0b1af1f929607f42adb704af03addb284624c374.

--- Shader Assignment/Assets/scripts/GrayScale.cs
+++ Shader Assignment/Assets/scripts/GrayScale.cs
@@ -6,17 +6,8 @@ using UnityEngine.Rendering.PostProcessing;
 [PostProcess(typeof(GrayscaleRenderer), PostProcessEvent.AfterStack, "Custom/Grayscale")]
 public sealed class Grayscale : PostProcessEffectSettings
 {
-    [Range(0f, 1f), Tooltip("Total effect intensity.")]
+    [Range(0f, 1f), Tooltip("Grayscale effect intensity.")]
     public FloatParameter blend = new FloatParameter { value = 0.5f };
-
-     [Range(0f, 1f), Tooltip("Red effect intensity.")]
-    public FloatParameter red = new FloatParameter { value = 0.5f };
-
-     [Range(0f, 1f), Tooltip("Green effect intensity.")]
-    public FloatParameter green = new FloatParameter { value = 0.5f };
-
-     [Range(0f, 1f), Tooltip("Blue effect intensity.")]
-    public FloatParameter blue = new FloatParameter { value = 0.5f };
 }
 
 public sealed class GrayscaleRenderer : PostProcessEffectRenderer<Grayscale>
@@ -25,10 +16,6 @@ public sealed class GrayscaleRenderer : PostProcessEffectRenderer<Grayscale>
     {
         var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Grayscale"));
         sheet.properties.SetFloat("_Blend", settings.blend);
-        sheet.properties.SetFloat("_red", settings.red);
-        sheet.properties.SetFloat("_green", settings.green);
-        sheet.properties.SetFloat("_blue", settings.blue);
-
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
     }
 }
